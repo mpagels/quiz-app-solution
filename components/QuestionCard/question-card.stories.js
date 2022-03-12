8d86f7a340995762d442b22cf4da2f571f2d6160
Martin Pagels <office@martinpagels.de>
2022-03-12 14:27:37 +0100

    Add question-card html markup

---
+++ components/QuestionCard/question-card.stories.js
@@ -0,0 +1,8 @@
+import "./question-card.css";
+import questionCard from "./question-card.html";
+
+export default {
+  title: "Components/QuestionCard",
+};
+
+export const questionCardClosed = () => questionCard;
