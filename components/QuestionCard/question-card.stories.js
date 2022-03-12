208a9bde2c6d0d11fdc2b9669d96b5a8f0e1cfb1
Martin Pagels <office@martinpagels.de>
2022-03-12 14:52:22 +0100

    Add question-card-open component

--- components/QuestionCard/question-card.stories.js
+++ components/QuestionCard/question-card.stories.js
@@ -1,8 +1,10 @@
 import "./question-card.css";
 import questionCard from "./question-card.html";
+import questionCardOpen from "./question-card-open.html";
 
 export default {
   title: "Components/QuestionCard",
 };
 
 export const questionCardClosed = () => questionCard;
+export const questionCardOpened = () => questionCardOpen;
